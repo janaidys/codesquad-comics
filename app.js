6358d897ca9c281246d44bb1e9cc96a981b662a8
Anaidys Jimenez <u.anaidys@gmail.com>
2024-04-02 17:45:52 -0400

    added get routes

---
+++ app.js
@@ -0,0 +1,42 @@
+// Require dependencies 
+const express = require('express');
+const morgan = require('morgan');
+
+
+//variables
+const app = express();
+const PORT = 3000; 
+
+//middleware
+
+app.use(morgan());
+
+// 5 GET paths 
+
+app.get("/", (request, response, next) => {
+    //response.send("This route points to the Home page")
+    response.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
+});
+
+app.get("/about", (request, response, next) => {
+    //response.send("This route points to the About page")
+    response.status(200).json({success: {message: "This route points to the About page"}, statusCode: 200});
+});
+
+app.get("/login", (request, response, next) => {
+    //response.send("This route points to the Login page")
+    response.status(200).json({success: {message: "This route points to the Login page"}, statusCode: 200});
+});
+
+app.get("/admin", (request, response, next) => {
+    //response.send("This route points to the Admin page")
+    response.status(200).json({success: {message: "This route points to the Admin page"}, statusCode: 200});
+});
+
+app.get("/admin/create-book", (request, response, next) => {
+    //response.send("This route points to the Create Book page")
+    response.status(200).json({success: {message: "This route points to the Create Bok page"}, statusCode: 200});
+});
+
+
+
