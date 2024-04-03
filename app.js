ef0087891e0d5a82decba95a125b65ecada5216d
Anaidys Jimenez <u.anaidys@gmail.com>
2024-04-03 17:10:00 -0400

    completed hw

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Require dependencies 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors')
 
 
 //variables
@@ -10,6 +12,11 @@ const PORT = 3000;
 //middleware
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// public directory is available
+app.use(express.static(path.join(__dirname + "/public")));
 
 // 5 GET paths 
 
@@ -18,25 +25,48 @@ app.get("/", (request, response, next) => {
     response.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
 });
 
-app.get("/about", (request, response, next) => {
-    //response.send("This route points to the About page")
-    response.status(200).json({success: {message: "This route points to the About page"}, statusCode: 200});
-});
+// app.get("/about", (request, response, next) => {
+//     //response.send("This route points to the About page")
+//     response.status(200).json({success: {message: "This route points to the About page"}, statusCode: 200});
+// });
 
-app.get("/login", (request, response, next) => {
-    //response.send("This route points to the Login page")
-    response.status(200).json({success: {message: "This route points to the Login page"}, statusCode: 200});
-});
+// app.get("/login", (request, response, next) => {
+//     //response.send("This route points to the Login page")
+//     response.status(200).json({success: {message: "This route points to the Login page"}, statusCode: 200});
+// });
 
-app.get("/admin", (request, response, next) => {
-    //response.send("This route points to the Admin page")
-    response.status(200).json({success: {message: "This route points to the Admin page"}, statusCode: 200});
-});
+// app.get("/admin", (request, response, next) => {
+//     //response.send("This route points to the Admin page")
+//     response.status(200).json({success: {message: "This route points to the Admin page"}, statusCode: 200});
+// });
 
-app.get("/admin/create-book", (request, response, next) => {
-    //response.send("This route points to the Create Book page")
-    response.status(200).json({success: {message: "This route points to the Create Bok page"}, statusCode: 200});
-});
+// app.get("/admin/create-book", (request, response, next) => {
+//     //response.send("This route points to the Create Book page")
+//     response.status(200).json({success: {message: "This route points to the Create Book page"}, statusCode: 200});
+// });
+
+// 5 NEW GET routes 
+
+app.get("/api/books", (request, response, next) => {
+    response.status(200).json({success: {message: "This will send all of the book data"}, statusCode: 200});
+    });
+
+app.get("/api/books/:id", (request, response, next) => {
+    response.status(200).json({success: {message: "This will send all of the books details data, or each book by their ID"}, statusCode: 200});
+     });
+
+app.get("/api/books/create/new", (request, response, next) => {
+    response.status(200).json({success: {message: "This will send all of the data that will have the ability to create new books"}, statusCode: 200});
+    });
+
+app.get("/api/books/edit/:id", (request, response, next) => {
+    response.status(200).json({success: {message: "This will send all of the update comic book form page data to modify a book by their ID"}, statusCode: 200});
+    });
+
+app.get("/api/books/delete/:id", (request, response, next) => {
+    response.status(200).json({success: {message: "This will send all of the data that will have the ability to delete a book by their ID"}, statusCode: 200});
+    });
+
 
 // Server
 app.listen(PORT, () => {
