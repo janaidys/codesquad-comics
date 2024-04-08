4231b2e23c1d626abd469d6a5b464d968cf65c3d
Anaidys Jimenez <u.anaidys@gmail.com>
2024-04-08 17:55:34 -0400

    completed CCS-3 homework. server is crashing because of the app.use

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-// Require dependencies 
+// packages 
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors')
-
-
-//variables
 const app = express();
 const PORT = 3000; 
+
+// define routing variables 
+const bookRoutes = require('./routes/bookRoutes');
+// const authRoutes = require('./routes/authRoutes');
 
 //middleware
 
@@ -18,12 +19,20 @@ app.use(express.urlencoded({ extended: false }));
 // public directory is available
 app.use(express.static(path.join(__dirname + "/public")));
 
+
+
 // 5 GET paths 
 
+// Should I be using this one (homework CCS-1)
 app.get("/", (request, response, next) => {
     //response.send("This route points to the Home page")
     response.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
 });
+
+// Should I be using this one (homework CCS-3)
+// app.get('/', (request, response, next) => {
+//     response.status(200).json({success: {message: "Index successful"}, statusCode: 200});
+//   })
 
 // app.get("/about", (request, response, next) => {
 //     //response.send("This route points to the About page")
@@ -45,28 +54,10 @@ app.get("/", (request, response, next) => {
 //     response.status(200).json({success: {message: "This route points to the Create Book page"}, statusCode: 200});
 // });
 
-// 5 NEW GET routes 
 
-app.get("/api/books", (request, response, next) => {
-    response.status(200).json({success: {message: "This will send all of the book data"}, statusCode: 200});
-    });
 
-app.get("/api/books/:id", (request, response, next) => {
-    response.status(200).json({success: {message: "This will send all of the books details data, or each book by their ID"}, statusCode: 200});
-     });
-
-app.get("/api/books/create/new", (request, response, next) => {
-    response.status(200).json({success: {message: "This will send all of the data that will have the ability to create new books"}, statusCode: 200});
-    });
-
-app.get("/api/books/edit/:id", (request, response, next) => {
-    response.status(200).json({success: {message: "This will send all of the update comic book form page data to modify a book by their ID"}, statusCode: 200});
-    });
-
-app.get("/api/books/delete/:id", (request, response, next) => {
-    response.status(200).json({success: {message: "This will send all of the data that will have the ability to delete a book by their ID"}, statusCode: 200});
-    });
-
+// Route Paths
+// app.use('/', authRoutes);
 
 // Server
 app.listen(PORT, () => {
