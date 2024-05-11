99a392d51092803e5d1a5e53911c7d5011e068b8
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-11 11:00:12 -0400

    working on CCS-7

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,5 +20,38 @@ success: {message: "User logged out!"}, statusCode:200,
 };
 
 const signupRequest = (request, response, next) => {
-  const {firstName, lastName, username, password}  = request.body
-}
+  const {firstName, lastName, username, password}  = request.body;
+
+  bcrypt.hash(password, 10, async (error, hashedPassword) => { 
+    if(error) {
+        return next(error);
+    }
+    const newUser = new User ({ 
+        firstName, 
+        lastName, 
+        username,
+        password: hashedPassword,
+    });
+    try {
+        await newUser.save();
+        request.login(newUser, (error) => { 
+            if (error) {
+                response.status(400).json({error: {message: "Something went wrong while signing up!"}, statusCode:400,
+            });
+        }
+    });   
+    } catch (error) {
+        if(error.code === 11000 && error.keyPattern.username) {
+            response.status(400).json({error: {message: "Username already exists."}, statusCode: 400, 
+        });
+        }else {
+            response.status(500).json({error: {message: "Internal server error"}, statusCode: 500,
+        });
+        }
+        
+    } 
+});
+};
+
+
+module.exports = {loginLocalFailed, logoutRequest, signupRequest};
