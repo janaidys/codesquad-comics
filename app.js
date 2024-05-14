e8468c342e04bafa480e5d80faa62b7e193748a8
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-13 20:14:59 -0400

    editted app.js

--- app.js
+++ app.js
@@ -47,9 +47,25 @@ app.use(express.static(path.join(__dirname + "/public")));
 // 5 GET paths
 
 app.get("/", (request, response, next) => {
-  response
-    .status(200)
-    .json({ success: { message: "Index successful" }, statusCode: 200 });
+  // response
+  //   .status(200)
+  //   .json({ success: { message: "Index successful" }, statusCode: 200 });
+  response.send(
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta   name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    </head>
+    <body>
+    <h1>Test for Deployment</h1>
+    <p>Index successful!</p>
+    </body>
+    </html>
+    `
+  )
 });
 
 app.get("/about", (request, response, next) => {
