7cdac0c06222f2d0d2a8fc55f2699d0798f4ed8b
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-23 23:03:29 -0400

    storing any minor changes

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,19 +1,19 @@
 // const booksData = require("/data/data.js");
 
 //Summoning the book model?
-const bookModel = require('../models/bookModel')
+const bookModel = require("../models/bookModel");
 
 // get ALL books
 const getAllBooks = async (request, response, next) => {
   try {
     if (200) {
-      await Book.find({})
-        .then(books=> 
+      await Book.find({}).then((books) =>
         response.status(200).json({
-        success: { message: "Found all books!" },
-        data: books,
-        statusCode: 200,
-      }));
+          success: { message: "Found all books!" },
+          data: books,
+          statusCode: 200,
+        })
+      );
     }
   } catch (error) {
     response.status(400).json({
@@ -25,17 +25,17 @@ const getAllBooks = async (request, response, next) => {
 
 // get ONE book
 const getBook = async (request, response, next) => {
-  const {id} = request.params;
-//   const foundBook = booksModel.find((book) => book.id === Number(id));
+  const { id } = request.params;
+  //   const foundBook = booksModel.find((book) => book.id === Number(id));
 
   try {
-      await Book.findOne({_id:id})
-      .then(foundBook => {
+    await Book.findOne({ _id: id }).then((foundBook) => {
       response.status(200).json({
         success: { message: "Found the book you are looking for!" },
         data: foundBook,
-        statusCode: 200});
-      })
+        statusCode: 200,
+      });
+    });
   } catch (error) {
     response.status(400).json({
       error: { message: "Something went wrong retrieving a book!" },
@@ -46,66 +46,89 @@ const getBook = async (request, response, next) => {
 
 // create a book
 const createBook = async (request, response, next) => {
-    const { title, author, genre, pages, rating, synopsis } = request.body;
+  const { title, author, genre, pages, rating, synopsis } = request.body;
 
-    const newBook = new Book({
-        title: title,
-        author: author,
-        genre: genre,
-        pages: pages,
-        rating: rating,
-        synopsis: synopsis,
+  const newBook = new Book({
+    title: title,
+    author: author,
+    genre: genre,
+    pages: pages,
+    rating: rating,
+    synopsis: synopsis,
+  });
+
+  await newBook.save();
+
+  try {
+    response
+      .status(201)
+      .json({
+        success: "A new book is created",
+        data: newBook,
+        statusCode: 201,
       });
-    
-    await newBook.save();
-
-    try {
-        response.status(201).json({success: "A new book is created", data: newBook, statusCode: 201 });
-    } catch (error) {
-        response.status(400).json({error: "Something went wrong creating a book", statusCode: 400 });
-    }
+  } catch (error) {
+    response
+      .status(400)
+      .json({ error: "Something went wrong creating a book", statusCode: 400 });
+  }
 };
 
-// edit a book 
+// edit a book
 const editBook = async (request, response, next) => {
-    const {id} = request.params; 
+  const { id } = request.params;
 
-    const { title, author, genre, pages, rating, synopsis } = request.body;
+  const { title, author, genre, pages, rating, synopsis } = request.body;
 
-    await Book.findByIdAndUpdate({id}, {
-        title,
-        author,
-        publisher,
-        genre,
-        pages,
-        rating,
-        synopsis
-        }, {new: true});
-    
-        try {
-            response.status(201).json({
-                success: 'Book is updated',
-                data: newBook,
-                statusCode: 201
-              });
-        } catch (error) {
-            response.status(400).json({ error: "Something went wrong while editing the book", statusCode: 400});
-        }
-}
+  await Book.findByIdAndUpdate(
+    { id },
+    {
+      title,
+      author,
+      publisher,
+      genre,
+      pages,
+      rating,
+      synopsis,
+    },
+    { new: true }
+  );
 
-// delete a book 
+  try {
+    response.status(201).json({
+      success: "Book is updated",
+      data: newBook,
+      statusCode: 201,
+    });
+  } catch (error) {
+    response
+      .status(400)
+      .json({
+        error: "Something went wrong while editing the book",
+        statusCode: 400,
+      });
+  }
+};
+
+// delete a book
 
 const deleteBook = async (request, response, next) => {
-    const {id} = request.params; 
+  const { id } = request.params;
 
-    await Book.findByIdAndDelete({id});
+  await Book.findByIdAndDelete({ id });
 
-    try {
-        response.status(200).json({success:"Book deleted successfully!", statusCode: 200})
-    } catch (error) {
-        response.status(400).json({error:"Something went wrong while deleting the book!", statusCode:400})
-        
-    }
-}
+  try {
+    response
+      .status(200)
+      .json({ success: "Book deleted successfully!", statusCode: 200 });
+  } catch (error) {
+    response
+      .status(400)
+      .json({
+        error: "Something went wrong while deleting the book!",
+        statusCode: 400,
+      });
+  }
+};
 
-module.exports = {getAllBooks, getBook, createBook, editBook, deleteBook}
+module.exports = { getAllBooks, getBook, createBook, editBook, deleteBook };
